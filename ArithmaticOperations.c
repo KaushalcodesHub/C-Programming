9f842c86c0257cf2270a377d70c3f44825cbd228
KaushalcodesHub <kaushalr.tech@gmail.com>
2024-06-25 00:10:26 +0530

    Create ArithmaticOperations.c
    Arithmatic operations on two numbers

---
+++ ArithmaticOperations.c
@@ -0,0 +1,15 @@
+//Write a program to arithmatic operation on two numbers
+#include<stdio.h>
+int main(){
+int num1,num2;
+printf("enter first number : ");
+scanf("%d",&num1);
+printf("enter Second number : ");
+scanf("%d",&num2);
+printf("\n the sum of the number is : %d ",a+b);
+printf("\n the substraction of the number is : %d ",a-b);
+printf("\n the multiplication of the number is : %d ",a*b);
+printf("\n the division of the number is : %d ",a/b);
+printf("\n the remainder of the number is : %d ",a%b);
+return 0;
+}
