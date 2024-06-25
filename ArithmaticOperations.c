6b41f6b7aa8017611febf87a7017e141be69d1d3
KaushalCodeshub <kaushalr.tech@gmail.com>
2024-06-25 19:26:41 +0530

    remove error

--- ArithmaticOperations.c
+++ ArithmaticOperations.c
@@ -6,10 +6,10 @@ printf("enter first number : ");
 scanf("%d",&num1);
 printf("enter Second number : ");
 scanf("%d",&num2);
-printf("\n the sum of the number is : %d ",a+b);
-printf("\n the substraction of the number is : %d ",a-b);
-printf("\n the multiplication of the number is : %d ",a*b);
-printf("\n the division of the number is : %d ",a/b);
-printf("\n the remainder of the number is : %d ",a%b);
+printf("\n the sum of the number is : %d ",num1+num2);
+printf("\n the substraction of the number is : %d ",num1-num2);
+printf("\n the multiplication of the number is : %d ",num1*num2);
+printf("\n the division of the number is : %d ",num1/num2);
+printf("\n the remainder of the number is : %d ",num1%num2);
 return 0;
 }
